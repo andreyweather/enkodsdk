6e21367246bbb5af7a555d5c2c8f2577f5d63de4
Andrey Pogodin <andrey.pogodin@enkod.io>
2024-02-14 15:11:39 +0300

    Commit

---
+++ enkodpushlibrary/src/main/java/com/enkod/enkodpushlibrary/Preferences.kt
@@ -0,0 +1,16 @@
+package com.enkod.enkodpushlibrary
+
+object Preferences {
+
+    internal const val TAG = "EnkodPushLibrary"
+    internal const val WORKER_TAG: String = "${TAG}_WORKER"
+    internal const val START_TIMER_TAG: String = "${TAG}_STARTTIMER"
+    internal const val TIME_TAG: String = "${TAG}_TIME"
+    internal const val SESSION_ID_TAG: String = "${TAG}_SESSION_ID"
+    internal const val TOKEN_TAG: String = "${TAG}_TOKEN"
+    internal const val ACCOUNT_TAG: String = "${TAG}_ACCOUNT"
+    internal const val MESSAGEID_TAG = "${TAG}_MESSAGEID"
+    internal const val EXIT_TAG: String = "${TAG}_EXIT"
+    internal const val DEV_TAG: String = "${TAG}_DEV"
+
+}
